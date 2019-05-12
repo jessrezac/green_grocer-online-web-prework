1f52bf0c1a799560fa6adc38f495d64397597e4b
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 01:13:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,9 +37,10 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  binding.pry
+
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   clearanced_cart = apply_clearance(couponed_cart)
-  
-  binding.pry
+
 end

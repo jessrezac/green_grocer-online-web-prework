5320dea638a7a3522f42cb576a44711d9f298676
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 01:30:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ def checkout(cart, coupons)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   clearanced_cart = apply_clearance(couponed_cart)
 
-  clearanced_cart.map do |product, attributes|
+  total = clearanced_cart.map do |product, attributes|
     attributes = attributes[:price] * attributes[:count]
     attributes
   end.sum

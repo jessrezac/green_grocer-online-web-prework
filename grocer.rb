7cfae63086e9910983d1bb3a5da0c5628cdd2997
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 01:09:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,43 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  cart.each_with_object({}) do |item, consolidated_cart|
+    item.each do |product, attributes|
+      if !consolidated_cart.include?(product)
+        consolidated_cart[product] = attributes
+        consolidated_cart[product][:count] = 1
+      else
+        consolidated_cart[product][:count] += 1
+      end
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+      if cart.include?("#{coupon[:item]} W/COUPON".upcase) && cart[coupon[:item]][:count] >= coupon[:num]
+        cart[coupon[:item]][:count] -= coupon[:num]
+        cart["#{coupon[:item]} W/COUPON".upcase][:count] += 1
+      elsif cart.include?(coupon[:item]) && cart[coupon[:item]][:count] >= coupon[:num]
+        cart[coupon[:item]][:count] -= coupon[:num]
+        cart["#{coupon[:item]} W/COUPON".upcase] = {:price => coupon[:cost], :clearance => cart[coupon[:item]][:clearance], :count => 1}
+      end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # code here
+  cart.map do |product, attributes|
+    if attributes[:clearance] == true
+      attributes[:price] = (attributes[:price]*0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  binding.pry
+
+  consolidated_cart = consolidate_cart(cart)
 end

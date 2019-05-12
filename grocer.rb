c2193d95d0c82257eb28b8efe2df9223ceca0658
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 01:27:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ def checkout(cart, coupons)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   clearanced_cart = apply_clearance(couponed_cart)
 
-  clearanced_cart.each_with_object({}) do |(product, attributes), total|
+  total = clearanced_cart.each_with_object({}) do |product, attributes|
     attributes[:price] * attributes[:count]
   end
   binding.pry
